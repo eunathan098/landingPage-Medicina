88cf1165d130a3f857d64e39749322e48286f6fe
Nathan <145821197+eunathan098@users.noreply.github.com>
2024-12-20 23:23:43 -0300

    atualização

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,16 +1,38 @@
-
 let isMenuOpen = false;
 
 function openMenuMobile() {
     const menuMobile = document.getElementById("bg-container-menuMobile");
-    
+    const visibilyButton = document.getElementById("btn-fechar-menu-Mobile");
+
+    if (visibilyButton) {
+        visibilyButton.style.display = "block";
+    }
+
     if (isMenuOpen) {
-        // Adiciona a classe que fecha o menu
         menuMobile.classList.remove("open");
     } else {
-        // Adiciona a classe que abre o menu
         menuMobile.classList.add("open");
     }
 
     isMenuOpen = !isMenuOpen;
+}
+
+function fecharMenuMobile() {
+    const menuMobile = document.getElementById("bg-container-menuMobile");
+    const aparecaButao = document.getElementById("btn-fechar-menu-Mobile");
+    const btnOpentoMenu = document.getElementById("icone-menu-lateral");
+
+    if (menuMobile) {
+        menuMobile.classList.remove("open");
+        isMenuOpen = false;
+    }
+
+    // Esconde o botão de fechar
+    if (aparecaButao) {
+        aparecaButao.style.display = "none";
+    }
+
+    if (btnOpentoMenu) {
+        btnOpentoMenu.style.display = "block";
+    }
 }
